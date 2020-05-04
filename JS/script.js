9a6c0baf81d6bc147da11c45883d5aa33c5b1120
gookudog321 <60128077+gookudog321@users.noreply.github.com>
2020-05-04 15:54:21 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".hamburger").on("click", function(){
+        $(".nav-grid").toggleClass("open");
+    })
+})
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
